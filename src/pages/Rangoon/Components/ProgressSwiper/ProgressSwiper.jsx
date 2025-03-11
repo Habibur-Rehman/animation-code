c43e92c7fc839fe3bebb0480e27d74043e9b931c
Habibur-Rehman <habibur@togglehead.in>
2025-03-11 15:47:17 +0530

    progress slider completed

--- src/pages/Rangoon/Components/ProgressSwiper/ProgressSwiper.jsx
+++ src/pages/Rangoon/Components/ProgressSwiper/ProgressSwiper.jsx
@@ -1,21 +1,80 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./progressSwiper.scss";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/effect-cards";
 
 // import required modules
 import { Autoplay, Navigation } from "swiper/modules";
-import { animateCircle, leftArrow, rightArrow } from "../../../../source";
+import { animateCircle, leftArrow, printedPage, rightArrow } from "../../../../source";
+import gsap from "gsap";
+
+const sliderData = [
+  {
+    id: 1,
+    img: animateCircle,
+    desc: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, corrupti distinctio, fugiat eum numquam sapiente expedita quaerat exercitationem voluptatem, unde laborum debitis. Dignissimos
+            praesentium accusantium consectetur reprehenderit reiciendis ut quibusdam.`,
+  },
+  {
+    id: 2,
+    img: printedPage,
+    desc: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, corrupti distinctio, fugiat eum numquam sapiente expedita quaerat exercitationem voluptatem, unde laborum debitis. Dignissimos
+            `,
+  },
+  {
+    id: 3,
+    img: animateCircle,
+    desc: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, corrupti distinctio, fugiat eum numquam sapiente expedita quaerat exercitationem voluptatem, unde laborum debitis. Dignissimos
+            praesentium accusantium consectetur reprehenderit reiciendis ut quibusdam.`,
+  },
+  {
+    id: 4,
+    img: printedPage,
+    desc: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, corrupti distinctio, fugiat eum numquam sapiente expedita quaerat exercitationem voluptatem, unde laborum debitis. Dignissimos
+            `,
+  },
+];
 
 const ProgressSwiper = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const progressRef = useRef(null);
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    if (!progressRef.current) return;
+
+    const updateProgress = () => {
+      gsap.to(progressRef.current, {
+        scaleX: 1,
+        duration: 5,
+        ease: "linear",
+      });
+    };
+
+    const resetProgress = () => {
+      gsap.set(progressRef.current, { scaleX: 0 });
+      updateProgress();
+    };
+
+    resetProgress();
+
+    return () => {
+      gsap.killTweensOf(progressRef.current);
+    };
+  }, [activeIndex]);
+
   return (
     <>
       <div className="progress_slider_container">
         <div className="slider_wrapper">
           <Swiper
+            ref={swiperRef}
+            // effect="cards"
             slidesPerView={1}
+            loop={true}
             navigation={{
               nextEl: ".right_arrow",
               prevEl: ".left_arrow",
@@ -24,15 +83,17 @@ const ProgressSwiper = () => {
               delay: 5000,
               disableOnInteraction: false,
             }}
-            modules={[Navigation,Autoplay]}
+            modules={[Navigation, Autoplay]}
             className="mySwiper"
+            onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
           >
-            <SwiperSlide>
-              <div className="box">
-                <div className="box_data">
-                  <img src={animateCircle} alt="" />
-                </div>
-                <div className="box_data content_wrapper">
+            {sliderData.map((item, i) => (
+              <SwiperSlide>
+                <div className="box" key={i}>
+                  <div className="box_data">
+                    <img src={item.img} alt="" />
+                  </div>
+                  {/* <div className="box_data content_wrapper">
                   <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Tempora, corrupti distinctio, fugiat eum numquam sapiente
@@ -40,61 +101,49 @@ const ProgressSwiper = () => {
                     debitis. Dignissimos praesentium accusantium consectetur
                     reprehenderit reiciendis ut quibusdam.
                   </p>
+                </div> */}
                 </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="box">
-                <div className="box_data">
-                  <img src={animateCircle} alt="" />
-                </div>
-                <div className="box_data content_wrapper">
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Tempora, corrupti distinctio, fugiat eum numquam sapiente
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="box">
-                <div className="box_data">
-                  <img src={animateCircle} alt="" />
-                </div>
-                <div className="box_data content_wrapper">
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Tempora, corrupti distinctio, fugiat eum numquam sapiente
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="box">
-                <div className="box_data">
-                  <img src={animateCircle} alt="" />
-                </div>
-                <div className="box_data content_wrapper">
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Tempora, corrupti distinctio, fugiat eum numquam sapiente
-                    expedita quaerat exercitationem voluptatem, unde laborum
-                    debitis. Dignissimos praesentium accusantium consectetur
-                    reprehenderit reiciendis ut quibusdam.
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
+            ))}
           </Swiper>
-          <div className="arrow_wrapper">
+          <div className="content_container">
+            {sliderData.map((item, i) => (
+              <div
+                className={`content_wrapper ${
+                  activeIndex === i ? "active" : ""
+                }`}
+                key={i}
+              >
+                <p>{item.desc}</p>
+              </div>
+            ))}
+            <div className="arrow_wrapper">
+              <img
+                src={leftArrow}
+                alt=""
+                className="left_arrow"
+                onClick={() => {
+                  swiperRef.current.swiper.slidePrev();
+                  setActiveIndex(swiperRef.current.swiper.realIndex);
+                }}
+              />
+              <img
+                src={rightArrow}
+                alt=""
+                className="right_arrow"
+                onClick={() => {
+                  swiperRef.current.swiper.slideNext();
+                  setActiveIndex(swiperRef.current.swiper.realIndex);
+                }}
+              />
+            </div>
+          </div>
+          {/* <div className="arrow_wrapper">
             <img src={leftArrow} alt="" className="left_arrow" />
             <img src={rightArrow} alt="" className="right_arrow" />
-          </div>
+          </div> */}
           <div className="slider__progress">
-            <div className="progress"></div>
+            <div className="progress" ref={progressRef}></div>
           </div>
         </div>
       </div>
