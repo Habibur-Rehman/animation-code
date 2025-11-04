803c2412c7f3d08f09139b80537b2059eec6d712
Habibur-Rehman <habibur@togglehead.in>
2025-11-04 16:47:04 +0530

    tractor animation updated

--- src/pages/Pfeda/TractorAnim/TractorAnim.jsx
+++ src/pages/Pfeda/TractorAnim/TractorAnim.jsx
@@ -13,7 +13,10 @@ import {
   tractor05Img,
   tractor06Img,
   tractorBgImg,
+  tractorBgVideo,
+  tractorDetailsImg,
 } from "../../../source";
+import { useWindowSize } from "react-use";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -25,6 +28,7 @@ gsap.registerPlugin(ScrollTrigger);
 const Tractor = () => {
   const { scene } = useGLTF("/Pfeda/tractor.glb");
   const tractorRef = useRef();
+  const { width } = useWindowSize();
 
   // Set initial rotation like in your reference image
   const initialRotation = [0, 0, 0]; // adjust as needed
@@ -33,41 +37,124 @@ const Tractor = () => {
     if (tractorRef.current) {
       const tl = gsap.timeline({
         scrollTrigger: {
-          trigger: "#tractor_section",
+          // trigger: "#tractor_section",
+          trigger: ".tract_sec",
           pin: true,
-          start: "0% top",
-          end: "200% 70%",
+          start: "top top",
+          end: "+=300%",
+          // start: "-5% top",
+          // end: "200% 70%",
           scrub: 1,
           pinSpacer: false,
+          // anticipatePin: 1,
           //   fastScrollEnd: 3000,
-        //   markers: true,
+          //   markers: true,
         },
       });
-      tl.to(tractorRef.current.rotation, {
-        y: Math.PI / 3,
-      });
+      tl.to(
+        tractorRef.current.rotation,
+        {
+          y: Math.PI / 3,
+        },
+        "same"
+      );
+      tl.fromTo(
+        ".title",
+        {
+          translateY: 0,
+          duration: 1,
+        },
+        {
+          translateY: "-100%",
+          duration: 1,
+        },
+        "same"
+      );
       tl.fromTo(
         ".details_container",
         {
           scale: 0,
-          transformOrigin: "50% 50%",
-          opacity: 0,
+          // opacity: 0,
+          duration: 1,
+          bottom: "50%",
+          transformOrigin: "bottom bottom",
         },
         {
           scale: 1,
-        //   transformOrigin: "bottom",
+          // opacity: 1,
+          duration: 1,
+          // bottom: "115%",
+          // bottom: `${width > 1024 ? "115%" : "110%"}`,
+          bottom: `${width > 1024 ? "55%" : "70%"}`,
+          // transformOrigin: "50% 50%",
+        },
+        "same"
+      );
+      tl.fromTo(
+        ".details_container",
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+        },
+        "-=0.6"
+      );
+      tl.fromTo(
+        "#tractor_section",
+        {
+          opacity: 1,
+        },
+        {
+          opacity: `${width > 991 ? 1 : 0}`,
+        },
+        `${width > 991 ? "0" : "+=0.15"}`
+      );
+      tl.fromTo(
+        ".content_wrapper",
+        {
+          translateY: "100%",
+          translateX: `${width > 991 ? 0 : "-50%"}`,
+          opacity: 0,
+          duration: 1,
+          bottom: 0,
+          // transformOrigin: "bottom bottom",
+        },
+        {
+          // translateY: "0%",
+          translateY: `${width > 991 ? "0%" : "-50%"}`,
+          translateX: `${width > 991 ? 0 : "-50%"}`,
           opacity: 1,
           duration: 1,
-
-          scrollTrigger: {
-            trigger: "#tractor_section",
-            start: "0.5% top",
-            end: "50% 70%",
-            scrub: 1,
-            markers: true,
-          },
-        }
+          // bottom: "14.5%",
+        },
+        `${width > 991 ? "-=0.61" : "=-0.15"}`
       );
+
+      // tl.fromTo(
+      //   ".details_container",
+      //   {
+      //     scale: 0,
+      //     transformOrigin: "50% 50%",
+      //     opacity: 0,
+      //   },
+      //   {
+      //     scale: 1,
+      //     //   transformOrigin: "bottom",
+      //     opacity: 1,
+      //     duration: 1,
+
+      //     scrollTrigger: {
+      //       trigger: ".tract_sec",
+      //       // start: "0.5% top",
+      //       start: "0.1% 0.05%",
+      //       end: "50% 50%",
+      //       // end: "+=300%",
+      //       scrub: 1,
+      //       markers: true,
+      //     },
+      //   }
+      // );
     }
   }, []);
 
@@ -89,63 +176,110 @@ const Tractor = () => {
   //       });
   //     }
   //   }, []);
+
+  const scaleValue = width > 1024 ? 95 : width > 767 ? 87 : 70;
+
   return (
     <primitive
       ref={tractorRef}
       object={scene}
-      scale={100}
+      scale={scaleValue}
       rotation={initialRotation}
     />
   );
 };
 
 export default function TractorAnim() {
+  const { width } = useWindowSize();
+
   return (
     <>
       <section className="tract_sec">
-        <h1 className="title">
-          Innovating Automotive <br /> & Furniture
-        </h1>
-        <div className="bg_img_wrapper">
-          <img src={tractorBgImg} className="tractor_bg" alt="" />
-          {/* <img src={tractorBgImg} className="tractor_bg" alt="" /> */}
-        </div>
-        <div className="my_container">
-          <div id="tractor_section">
-            {/* <div className="tract_wrapper"> */}
-            {/* <div className="sticky_wrapper"> */}
-            <Canvas camera={{ position: [5, 2, 0] }} className="tractor_canvas">
-              <ambientLight intensity={0.6} />
-              <directionalLight position={[3, 3, 3]} />
-              <Suspense fallback={null}>
-                <Tractor />
-              </Suspense>
-            </Canvas>
-            {/* </div> */}
+        <div className="tract_container">
+          <h1 className="title">
+            Innovating Automotive <br /> & Furniture
+          </h1>
+          {/* <div className="bg_img_wrapper">
+            <img src={tractorBgImg} className="tractor_bg" alt="" />
+          </div> */}
+          <div className="tractor_vid_wrapper">
+            <video
+              width="100%"
+              height="100%"
+              playsInline
+              muted
+              loop
+              autoPlay
+              preload="auto"
+              className="tractor_vid"
+            >
+              <source src={tractorBgVideo} type="video/mp4" />
+            </video>
+          </div>
 
-            <div className="details_container">
-              <div className="details_wrapper">
-                <div className="data_wrapper">
-                  <img className="tract_1" src={tractor01Img} alt="1" />
+          {/* <div className="my_container"> */}
+          <div>
+            <div id="tractor_section">
+              {/* <div className="tract_wrapper"> */}
+              {/* <div className="sticky_wrapper"> */}
+              <Canvas
+                camera={{ position: [5, 2, 0] }}
+                className="tractor_canvas"
+              >
+                <ambientLight intensity={0.6} />
+                <directionalLight position={[3, 3, 3]} />
+                <Suspense fallback={null}>
+                  <Tractor />
+                </Suspense>
+              </Canvas>
+              {/* </div> */}
+
+              {/* <div className="details_container">
+                <div className="details_wrapper">
+                  <div className="data_wrapper">
+                    <img className="tract_1" src={tractor01Img} alt="1" />
+                  </div>
+                  <div className="data_wrapper">
+                    <img className="tract_2" src={tractor02Img} alt="2" />
+                  </div>
+                  <div className="data_wrapper">
+                    <img className="tract_3" src={tractor03Img} alt="3" />
+                  </div>
+                  <div className="data_wrapper">
+                    <img className="tract_4" src={tractor04Img} alt="4" />
+                  </div>
+                  <div className="data_wrapper">
+                    <img className="tract_5" src={tractor05Img} alt="5" />
+                  </div>
+                  <div className="data_wrapper">
+                    <img className="tract_6" src={tractor06Img} alt="6" />
+                  </div>
                 </div>
+              </div> */}
+
+              <div className="details_container">
                 <div className="data_wrapper">
-                  <img className="tract_2" src={tractor02Img} alt="2" />
-                </div>
-                <div className="data_wrapper">
-                  <img className="tract_3" src={tractor03Img} alt="3" />
-                </div>
-                <div className="data_wrapper">
-                  <img className="tract_4" src={tractor04Img} alt="4" />
-                </div>
-                <div className="data_wrapper">
-                  <img className="tract_5" src={tractor05Img} alt="5" />
-                </div>
-                <div className="data_wrapper">
-                  <img className="tract_6" src={tractor06Img} alt="6" />
+                  <img
+                    src={tractorDetailsImg}
+                    alt="tractor details"
+                    className="tractor_details"
+                  />
                 </div>
               </div>
+              {/* </div> */}
             </div>
-            {/* </div> */}
+
+            <div className="content_wrapper">
+              <h3 className="section_title">ABOUT US</h3>
+              <h2 className="section_subTitle">Transforming With Innovation</h2>
+              <p className="section_desc">
+                PFEDA is a leading global manufacturer of Polyurethane based
+                products and solutions for the automotive industry and other
+                fields of applications with a portfolio that covers a wide range
+                of construction Equipments, Tractors, Trucks & Buses
+              </p>
+              <button className="section_btn">EXPLORE MORE</button>
+            </div>
           </div>
         </div>
       </section>
--- src/source/index.js
+++ src/source/index.js
@@ -98,6 +98,8 @@ export { default as tractor03Img } from "./images/Pfeda/3.png";
 export { default as tractor04Img } from "./images/Pfeda/4.png";
 export { default as tractor05Img } from "./images/Pfeda/5.png";
 export { default as tractor06Img } from "./images/Pfeda/6.png";
+export { default as tractorDetailsImg } from "./images/Pfeda/tractor-details.png";
+export { default as tractorBgVideo } from "./images/Pfeda/pfeda_vid.mp4";
 
 export let images = {
   archinzaGal01: {
